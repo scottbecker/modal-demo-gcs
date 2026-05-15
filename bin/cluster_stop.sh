2ff1f73881c4f633de55a4ac323634cff7d471fb
scottbecker <scott@example.com>
2026-05-15 09:03:41 -0400

    Make cluster stop non-interactive

--- bin/cluster_stop.sh
+++ bin/cluster_stop.sh
@@ -2,5 +2,5 @@
 # CLUSTER MODE: Stop/Undeploy the cluster
 PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )"
 echo "Stopping the Warm Cluster (modal app stop)..."
-"$PROJECT_ROOT/.venv/bin/modal" app stop px-container-demo
+"$PROJECT_ROOT/.venv/bin/modal" app stop -y px-container-demo
 echo "Cluster stopped."
